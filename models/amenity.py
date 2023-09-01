d008419271675c5154d6336ea32b551e79aa24f2
Browni Wilson <browniwils@gmail.com>
2023-09-01 21:03:21 +0000

    fix filestorage

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sql
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     """
     Model amenity
     """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    name = ''
+
